8f56b7421955d2cd765bbc50e2434c162ca1de6b
matthew <matthewdelannoy527@gmail.com>
2026-03-12 11:44:35 +0100

    Fully working gazebo test labs, added adjustments to core mirte packages in adjustments.txt

--- mirte_lc_labclean/mirte_lc_labclean/test_node.py
+++ mirte_lc_labclean/mirte_lc_labclean/test_node.py
@@ -13,23 +13,20 @@ class MirteTestController(Node):
             Twist, "/mirte_base_controller/cmd_vel_unstamped", 10
         )
         self.right_range_sub = self.create_subscription(
-            Range, "/mirte/distance/front_right", self.left_sensor_callback, 1
+            Range, "/mirte/distance/front_right", self.right_sensor_callback, 1
         )
         self.left_range_sub = self.create_subscription(
-            Range, "/mirte/distance/front_left", self.right_sensor_callback, 1
+            Range, "/mirte/distance/front_left", self.left_sensor_callback, 1
         )
         self.controller_timer = self.create_timer(0.05, self.controller_callback)
 
         self.distance_left = 0.01
         self.distance_right = 0.01
-        self.k_p_t_1 = 5
-        self.k_p_t_2 = 0.1
+        self.k_p_t = 20
         self.k_p_l = 1
 
     def controller_callback(self):
-        ang_vel = self.k_p_t_1 * (
-            self.distance_left - self.distance_right
-        ) + self.k_p_t_2 / (self.distance_left + self.distance_right)
+        ang_vel = self.k_p_t * (self.distance_left - self.distance_right)
         lin_vel = self.k_p_l * (self.distance_left + self.distance_right)
 
         self.get_logger().info(
@@ -38,20 +35,26 @@ class MirteTestController(Node):
         twist_msg = Twist()
 
         # Apply values to message (with clamping)
-        twist_msg.linear.y = -lin_vel if lin_vel < 0.6 else -0.6
-        twist_msg.angular.z = ang_vel if ang_vel < 2.0 else 2.0
+        twist_msg.linear.x = min(lin_vel, 0.6)
+        twist_msg.angular.z = min(ang_vel, 0.6)
 
         self.twist_pub.publish(twist_msg)
         return
     
     def left_sensor_callback(self, msg: Range):
         self.get_logger().info(f'Received {msg.range} from left')
-        self.distance_left = msg.range
+        try:
+            self.distance_left = min(msg.range, 0.4)
+        except Exception as e:
+            self.get_logger().error(f'{e}')
         return
     
     def right_sensor_callback(self, msg: Range):
         self.get_logger().info(f'Received {msg.range} from right')
-        self.distance_right = msg.range
+        try:
+            self.distance_right = min(msg.range, 0.4)
+        except Exception as e:
+            self.get_logger().error(f'{e}')
         return
 
 def main(args=None):
@@ -59,7 +62,7 @@ def main(args=None):
     rclpy.spin(MirteTestController())
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__== '__main__':
     main()
 
     
