f09c1a2fcd8ed7ed8a8e22d39bcefd5bc3a25d8f
matthew <matthewdelannoy527@gmail.com>
2026-06-06 23:59:21 +0200

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,20 +14,20 @@ author = 'Matthew de Lannoy'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.intersphinx',
+    # 'sphinx.ext.intersphinx',
 ]
 
 html_theme = 'sphinx_rtd_theme'
 
 # Link to each package's built docs for cross-referencing
-intersphinx_mapping = {
-    'mirte_lc_gazebo':      ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_gazebo', None),
-    'mirte_lc_labclean':    ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_labclean', None),
-    'mirte_lc_moveit_cpp':  ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_moveit_cpp', None),
-    'mirte_lc_msgs':        ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_msgs', None),
-    'mirte_lc_nav2':        ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_nav2', None),
-    'mirte_lc_vision':      ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_vision', None),
-}
+# intersphinx_mapping = {
+#     'mirte_lc_gazebo':      ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_gazebo', None),
+#     'mirte_lc_labclean':    ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_labclean', None),
+#     'mirte_lc_moveit_cpp':  ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_moveit_cpp', None),
+#     'mirte_lc_msgs':        ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_msgs', None),
+#     'mirte_lc_nav2':        ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_nav2', None),
+#     'mirte_lc_vision':      ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_vision', None),
+# }
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -36,6 +36,4 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
 html_static_path = ['_static']
