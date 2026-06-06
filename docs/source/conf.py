8efeeda4ab716df330ba71beac665546dced6c90
matthew <matthewdelannoy527@gmail.com>
2026-06-06 23:04:49 +0200

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,6 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-
 project = 'Lab Cleanup Robot using MIRTE Master Platform'
 copyright = '2026, Matthew de Lannoy'
 author = 'Matthew de Lannoy'
@@ -15,21 +13,22 @@ author = 'Matthew de Lannoy'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+extensions = [
+    'sphinx.ext.intersphinx',
+]
+
 html_theme = 'sphinx_rtd_theme'
 
-# Path from docs/source/ to the docs_output directory
-_docs = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../documentation/docs_output'))
-
-extensions = ['sphinx.ext.intersphinx']
-
+# Link to each package's built docs for cross-referencing
 intersphinx_mapping = {
-    'mirte_lc_gazebo':     ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_gazebo',    f'{_docs}/mirte_lc_gazebo/objects.inv'),
-    'mirte_lc_labclean':   ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_labclean',  f'{_docs}/mirte_lc_labclean/objects.inv'),
-    'mirte_lc_moveit_cpp': ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_moveit_cpp',f'{_docs}/mirte_lc_moveit_cpp/objects.inv'),
-    'mirte_lc_msgs':       ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_msgs',      f'{_docs}/mirte_lc_msgs/objects.inv'),
-    'mirte_lc_nav2':       ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_nav2',      f'{_docs}/mirte_lc_nav2/objects.inv'),
-    'mirte_lc_vision':     ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_vision',    f'{_docs}/mirte_lc_vision/objects.inv'),
+    'mirte_lc_gazebo':      ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_gazebo', None),
+    'mirte_lc_labclean':    ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_labclean', None),
+    'mirte_lc_moveit_cpp':  ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_moveit_cpp', None),
+    'mirte_lc_msgs':        ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_msgs', None),
+    'mirte_lc_nav2':        ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_nav2', None),
+    'mirte_lc_vision':      ('https://matt-rbt.github.io/Mirte_Lab_Clean/mirte_lc_vision', None),
 }
+
 templates_path = ['_templates']
 exclude_patterns = []
 
